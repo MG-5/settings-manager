760c0e48404b0ecb65a7760de0b9110094e46058
MG-5 <mg-95@t-online.de>
2023-01-09 06:54:53 +0100

    Add hash to every SettingsEntry

--- include/settings-manager/SettingsEntry.hpp
+++ include/settings-manager/SettingsEntry.hpp
@@ -1,4 +1,6 @@
 #pragma once
+
+#include "core/hash.hpp"
 #include <string_view>
 
 namespace settings
@@ -21,19 +23,20 @@ public:
     const SettingsValue_t maxValue;
     const std::string_view name;
     const VariableType variableType;
+    const uint64_t NameHash;
 
     //----------------------------------------------------------------------------------------------
     constexpr SettingsEntry(const SettingsValue_t min, const SettingsValue_t defaultValue,
                             const SettingsValue_t max, std::string_view name,
                             const VariableType variableType = VariableType::realType)
-        : minValue{min}, defaultValue{defaultValue}, maxValue{max}, name{name}, variableType{
-                                                                                    variableType}
+        : minValue{min}, defaultValue{defaultValue}, maxValue{max}, name{name},
+          variableType{variableType}, NameHash{core::hash::fnvStringview(name)}
     {
     }
 
     constexpr SettingsEntry(const bool defaultBoolValue, std::string_view name)
         : minValue{0}, defaultValue{defaultBoolValue ? 1.0f : 0.0f}, maxValue{1}, name{name},
-          variableType{VariableType::booleanType}
+          variableType{VariableType::booleanType}, NameHash{core::hash::fnvStringview(name)}
     {
     }
 
@@ -45,6 +48,11 @@ public:
     [[nodiscard]] constexpr bool hasSameName(const std::string_view &otherName) const
     {
         return name.compare(otherName) == 0;
+    }
+
+    [[nodiscard]] constexpr bool hasSameHash(const uint64_t &otherHash) const
+    {
+        return NameHash == otherHash;
     }
 };
 } // namespace settings
